d455cc46da8a6a63cbeb14d00c2d6788d24528c7
beaverDamDemo <zdenevi@gmail.com>
2025-11-23 21:22:40 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: 'export', // 👈 replaces next export
-  basePath: isProd ? '/english-lessons-maze' : '',
-  assetPrefix: isProd ? '/english-lessons-maze/' : '',
+  basePath: '/english-lessons-maze',
+  // assetPrefix: isProd ? '/english-lessons-maze/' : '',
   eslint: { ignoreDuringBuilds: true },
   images: {
     unoptimized: true, // 👈 required for static export
