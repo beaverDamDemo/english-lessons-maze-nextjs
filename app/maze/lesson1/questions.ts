c00d369d30f3502f03e63654cbface9789d2d849
beaverDamDemo <zdenevi@gmail.com>
2025-11-25 19:52:17 +0100

    moved questions out ot quiz file

---
+++ app/maze/lesson1/questions.ts
@@ -0,0 +1,128 @@
+export const questions = [
+  {
+    q: 'Lorem ipsum dolor sit amet. What is the correct answer?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Consectetur adipiscing elit. Which one is right?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Sed do eiusmod tempor. Pick the right one.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Incididunt ut labore et dolore. What should you pick?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Magna aliqua ut enim. Where is the answer?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Ad minim veniam quis. Find the correct one.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Nostrud exercitation ullamco. Pick the right choice.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Laboris nisi ut aliquip. Select the answer.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Ex ea commodo consequat. Which is correct?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Duis aute irure dolor. Pick this one.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'In reprehenderit in voluptate. Choose wisely.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Velit esse cillum dolore. What is the answer?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Eu fugiat nulla pariatur. Can you find it?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Excepteur sint occaecat cupidatat non. Pick correctly.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Proident sunt in culpa qui officia. Make your choice.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Deserunt mollit anim id est laborum. What is right?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Sed ut perspiciatis unde omnis iste. Pick the answer.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Natus error sit voluptatem accusantium. Choose one.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Doloremque laudantium totam rem aperiam. Which one?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Eaque ipsa quae ab illo inventore. Pick this.',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+  {
+    q: 'Veritatis et quasi architecto beatae vitae. Got it?',
+    options: ['Correct', 'Wrong', 'Wrong', 'Wrong'
+    ],
+    answer: 0,
+  },
+];
