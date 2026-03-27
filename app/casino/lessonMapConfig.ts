f3f71f5ec2eed5af7dacc8785917c7f60c435bbe
beaverDamDemo <zdenevi@gmail.com>
2026-03-27 12:34:18 +0100

    updates

--- app/casino/lessonMapConfig.ts
+++ app/casino/lessonMapConfig.ts
@@ -6,9 +6,9 @@ export type LessonMapButton = {
 };
 
 export const lessonMapButtons: LessonMapButton[] = [
-  { num: 1, color: '#264653', left: 0, top: 0 },
-  { num: 2, color: '#E63946', left: 9, top: 80 },
-  { num: 3, color: '#1D3557', left: 35.4, top: 58.6 },
-  { num: 4, color: '#2A9D8F', left: 47.7, top: 46.7 },
-  { num: 5, color: '#F4A261', left: 37.5, top: 22.9 },
+  { num: 1, color: '#264653', left: 57, top: 77.0 },
+  { num: 2, color: '#E63946', left: 35.4, top: 58.6, },
+  { num: 3, color: '#1D3557', left: 47.7, top: 46.7, },
+  { num: 4, color: '#2A9D8F', left: 37.5, top: 22.9, },
+  { num: 5, color: '#F4A261', left: 30.2, top: 34.6, },
 ];
